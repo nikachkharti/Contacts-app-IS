b96b95b4053322f8eceba74bfac748685aae1c87
ჩხარტიშვილი ნიკოლოზი <chkhartishvili.n@itstep.ge>
2025-04-28 10:38:19 +0400

    Project init

---
+++ Contacts-app-IdentityServer/Contacts-app-IdentityServer.API/Program.cs
@@ -0,0 +1,24 @@
+using Contacts_app_IdentityServer.API.Extensions;
+
+namespace Contacts_app_IdentityServer.API
+{
+    public class Program
+    {
+        public static void Main(string[] args)
+        {
+            var builder = WebApplication.CreateBuilder(args);
+
+            builder.AddControllers();
+            builder.AddSwagger();
+
+            var app = builder.Build();
+
+            app.UseSwagger();
+            app.UseSwaggerUI();
+            app.UseHttpsRedirection();
+            app.UseAuthorization();
+            app.MapControllers();
+            app.Run();
+        }
+    }
+}
